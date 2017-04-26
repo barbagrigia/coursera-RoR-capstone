5d0ad097e88ea71dc560e54126661a1445d69f08
Vlad Trukhin <vvt@narod.ru>
2017-04-26 16:26:32 +0400

    add sd-current-tags component

--- app/assets/javascripts/spa-demo.js
+++ app/assets/javascripts/spa-demo.js
@@ -6,6 +6,9 @@
 //= require angular-resource
 //= require angular-cookie
 //= require ng-token-auth
+//= require ng-file-upload-shim
+//= require ng-file-upload
+//= require ui-cropper
 
 //= require spa-demo/app.module
 //= require spa-demo/app.router
@@ -25,8 +28,23 @@
 //= require spa-demo/authz/authz.service
 //= require spa-demo/authz/base_policy.service
 
+//= require spa-demo/geoloc/geoloc.module
+//= require spa-demo/geoloc/geocoder.service
+//= require spa-demo/geoloc/map.service
+//= require spa-demo/geoloc/my_location.service
+// require spa-demo/geoloc/jhu_location.config
+//= require spa-demo/geoloc/current_origin/current_origin.service
+//= require spa-demo/geoloc/current_origin/current_origin_selector.component
+
 //= require spa-demo/layout/layout.module
+//= require spa-demo/layout/image_query_size.service
 //= require spa-demo/layout/navbar/navbar.component
+//= require spa-demo/layout/image_loader/data_utils.service
+//= require spa-demo/layout/image_loader/image_loader.component
+//= require spa-demo/layout/image_viewer/image_viewer.component
+
+//= require spa-demo/layout/tabs/tabs.component
+//= require spa-demo/layout/areas/areas.component
 
 //= require spa-demo/foos/foos.module
 //= require spa-demo/foos/foos.service
@@ -49,3 +67,9 @@
 //= require spa-demo/subjects/tags/tags.service
 //= require spa-demo/subjects/tags/tags.component
 //= require spa-demo/subjects/tags/tags_authz.service.js
+//= require spa-demo/subjects/current_tags/current_tags.component.js
+
+//= require spa-demo/subjects/current_subjects/current_subjects.service
+//= require spa-demo/subjects/current_subjects/current_subjects.component
+//= require spa-demo/subjects/current_images/current_images.component
+//= require spa-demo/subjects/current_things/current_things.component
